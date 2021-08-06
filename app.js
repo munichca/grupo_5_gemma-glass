d3f9388893ef788b443f2a63d061865f13bb6e09
AntonioDiazFierro <antoniodiazfierro.gmail.com>
2021-08-05 23:22:39 -0300

    detalle modificado y registro agregado

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 
 app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname, '/views/detalle.html'))
+    res.sendFile(path.join(__dirname, '/views/registro.html'))
 });
 
 
