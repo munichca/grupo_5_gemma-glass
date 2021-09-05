73019aae291ebf7fe8b139a50dc990618c360c72
AntonioDiazFierro <antoniodiazfierro.gmail.com>
2021-09-05 19:44:46 -0300

    img reparado

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 let express = require ("express");
 let router = express.Router();
 let controller = require ("../controllers/usersController");
+let uploadUserAvatar = require('../middlewares/uploadUserAvatar')
 /* GET RUta para login */
 router.get("/login", controller.login);
 router.get("/registro", controller.register);
+/* router.post('/registro', uploadUserAvatar.single('avatar'), registerValidator, processRegister); */
 router.get("/user", controller.user);
 router.get("/userEdit", controller.editProfileUser);
 
