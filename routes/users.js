4d96911d27ff619433d7e06552c12298433f2d21
AntonioDiazFierro <antoniodiazfierro.gmail.com>
2021-09-08 23:40:36 -0300

    acepta

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,5 @@
 let express = require ("express");
 let router = express.Router();
-<<<<<<< HEAD
-let controller = require ("../controllers/usersController");
-let uploadUserAvatar = require('../middlewares/uploadUserAvatar')
-/* GET RUta para login */
-router.get("/login", controller.login);
-router.get("/registro", controller.register);
-/* router.post('/registro', uploadUserAvatar.single('avatar'), registerValidator, processRegister); */
-router.get("/user", controller.user);
-router.get("/userEdit", controller.editProfileUser);
-=======
 let {login,  user, agregarUser, crearUser, editProfileUser} = require ("../controllers/usersController");
 let uploadUserAvatar = require('../middlewares/uploadUserAvatar')
 /* GET RUta para login */
@@ -19,7 +9,6 @@ router.get("/user", user);
 router.get("/agregarUser", agregarUser);
 router.post("/agregarUser", uploadUserAvatar.single("avatar"), crearUser);
 router.get("/userEdit", editProfileUser);
->>>>>>> antonio
 
 
 
