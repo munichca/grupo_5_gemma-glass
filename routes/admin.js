effa8779363ca9a3557d62ec8ce702ab8b892255
AntonioDiazFierro <antoniodiazfierro.gmail.com>
2021-09-19 17:27:15 -0300

    cambios

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ router.get("/edit/:id",userAdmin, edit);
 
 router.get("/listado",cookie,userAdmin ,lista);
 
-router.put("/edit/:id",edicion);
+router.put("/edit/:id", uploadProductImages.array("archivos"), edicion);
 router.delete("/eliminarProducto/:id", borrarProducto)
 router.get('/add', cookie,add);
 router.post('/add', uploadProductImages.array("archivos"), nuevoProducto);
