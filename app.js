254a135f6d1b491efc23d4e4f512a0137f32cf6b
evelyngerez <evelyngerez1603@gmail.com>
2021-10-14 14:13:00 -0300

    fix ruta middlewares

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3002;
 const path = require('path');
 let cookiepParser = require('cookie-parser')
 let methodOverride = require('method-override');
 let session = require('express-session');
-let localsCheck = require("./Middlewares/localsCheck")
+let localsCheck = require("./middlewares/localsCheck")
 let headerCateg = require("./middlewares/headerCateg")
 /*  ENRUTADORES */
 const homeRouter = require("./routes/home");
